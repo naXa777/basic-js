7b0e3cdd8707698fdd669ac902c26807f408e13f
Pavel <naxa@nextmail.ru>
2021-03-13 21:55:49 +0100

    feat: carbon dating with input validation

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,12 @@
-const CustomError = require("../extensions/custom-error");
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const LOG_2 = 0.693;
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string')
+    return false;
+  const activity = +sampleActivity;
+  if (activity > 0 && activity <= 15)
+    return Math.ceil(Math.log(MODERN_ACTIVITY / activity) / (LOG_2 / HALF_LIFE_PERIOD));
+  return false;
 };
