73cbb132e7a50b2671d05ebe71062fe24cbbe25e
Pavel <naxa@nextmail.ru>
2021-03-14 02:31:35 +0100

    feat: implement chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,30 @@
-const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
+  chain: [],
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+    if (value === null) value = 'null';
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (isNaN(+position) || position > this.chain.length || position < 1) {
+      this.chain = [];
+      throw Error(`Position out of bounds: ${position}.`);
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain = this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    const finalChain = this.chain.length > 0 ? '( ' + this.chain.join(' )~~( ') + ' )' : '';
+    this.chain = [];
+    return finalChain;
   }
 };
 
