c68aa9bda20d5f977a5e449b6a12013c2c13bebf
Pavel <naxa@nextmail.ru>
2021-03-14 01:14:20 +0100

    feat: transform array with control sequences
    passing 6 out of 8 tests

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,61 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr))
+    throw new CustomError('Not implemented');
+
+  console.log('input');
+  console.log(arr);
+
+  const transformed = [];
+  let doubleNext = false;
+  let skipNext = false;
+  let skippedPrev = false;
+  arr.forEach(e => {
+    switch (e) {
+      case '--discard-next':
+        skipNext = true;
+        break;
+      case '--discard-prev':
+        if (skippedPrev) {
+          // advanced: do not discard previous, if it was already discarded by '--discard-next'
+          skippedPrev = false;
+        } else {
+          transformed.pop();
+        }
+        break;
+      case '--double-next':
+        doubleNext = true;
+        break;
+      case '--double-prev':
+        if (skippedPrev) {
+          // advanced: do not double previous, if it was discarded by --discard-next
+          skippedPrev = false;
+        } else {
+          const prev = transformed.pop();
+          if (prev) {
+            transformed.push(prev);
+            transformed.push(prev);
+          }
+        }
+        break;
+      default:
+        if (!skipNext) {
+          transformed.push(e);
+          if (doubleNext) {
+            transformed.push(e);
+          }
+          skippedPrev = false;
+        } else {
+          skippedPrev = true;
+        }
+        skipNext = false;
+        doubleNext = false;
+    }
+  });
+
+  console.log('output');
+  console.log(transformed);
+
+  return transformed;
 };
