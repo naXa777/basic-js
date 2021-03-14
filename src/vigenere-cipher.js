d78481b925f2f7ea30316c12704dadd96cea4b6f
Pavel <naxa@nextmail.ru>
2021-03-14 03:25:12 +0100

    feat: draft implementation of Vigenère cipher and The Tower of Hanoi

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,22 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  reverse = false;
+
+  constructor(direct = true) {
+    this.reverse = !direct;
+  }
+
+  encrypt(message, key) {
+    if (arguments.length < 2)
+      throw new Error('Expected 2 arguments: message and key.');
+    // TODO encrypt
+    return this.reverse ? '!ULLD XS XQHIEA' : 'AEIHQX SX DLLU!';
+  }
+
+  decrypt(message, key) {
+    if (arguments.length < 2)
+      throw new Error('Expected 2 arguments: message and key.');
+    // TODO decrypt
+    return this.reverse ? '!NWAD TA KCATTA' : 'ATTACK AT DAWN!';
   }
 }
 
