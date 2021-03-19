c7a8a2bf9f68974e90e55090c22b1901690a8057
Pavel <naxa@nextmail.ru>
2021-03-19 01:18:49 +0100

    feat: implement The Tower of Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,3 +1,8 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    // TODO
+    const HOUR_IN_SECONDS = 60 * 60;
+    const turns = 2 ** disksNumber - 1;
+    return {
+        turns: turns,
+        seconds: Math.floor(HOUR_IN_SECONDS / turnsSpeed * turns),
+    }
 };
