d1dbad75a03cfe0c42d05ece0596e2b64007a1bf
Pavel <naxa@nextmail.ru>
2021-03-19 01:46:27 +0100

    feat: draft implementation of Vigenère cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,7 @@
 class VigenereCipheringMachine {
-  reverse = false;
+  ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  base = 'A'.charCodeAt(0);
+  max = this.ALPHABET.length;
 
   constructor(direct = true) {
     this.reverse = !direct;
@@ -8,15 +10,35 @@ class VigenereCipheringMachine {
   encrypt(message, key) {
     if (arguments.length < 2)
       throw new Error('Expected 2 arguments: message and key.');
-    // TODO encrypt
-    return this.reverse ? '!ULLD XS XQHIEA' : 'AEIHQX SX DLLU!';
+
+    const encrypted = message.toUpperCase()
+        .split('')
+        .map((letter, i) => {
+          if (!this.ALPHABET.includes(letter))
+            return letter;
+          const keyShift = key.charCodeAt(i % key.length) - this.base;
+          const shift = letter.charCodeAt(0) - this.base;
+          return this.ALPHABET[(keyShift + shift) % this.max];
+        });
+
+    return this.reverse ? encrypted.reverse().join('') : encrypted.join('');
   }
 
   decrypt(message, key) {
     if (arguments.length < 2)
       throw new Error('Expected 2 arguments: message and key.');
-    // TODO decrypt
-    return this.reverse ? '!NWAD TA KCATTA' : 'ATTACK AT DAWN!';
+
+    const decrypted = message.toUpperCase()
+        .split('')
+        .map((letter, i) => {
+          if (!this.ALPHABET.includes(letter))
+              return letter;
+          const keyShift = key.charCodeAt(i % key.length) - this.base;
+          const shift = letter.charCodeAt(0) - this.base;
+          return this.ALPHABET[(this.max + shift - keyShift) % this.max];
+        });
+
+    return this.reverse ? decrypted.reverse().join('') : decrypted.join('');
   }
 }
 
