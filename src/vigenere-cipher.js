34a9445a9ccc595f23d35a4476c10197c0e9b7c4
Pavel <naxa@nextmail.ru>
2021-03-19 12:42:59 +0100

    refactor: format code

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,59 +1,59 @@
 class VigenereCipheringMachine {
-  ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  base = 'A'.charCodeAt(0);
-  max = this.ALPHABET[this.ALPHABET.length - 1];
+    ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    base = 'A'.charCodeAt(0);
+    max = this.ALPHABET[this.ALPHABET.length - 1];
 
-  constructor(direct = true) {
-    this.reverse = !direct;
-  }
+    constructor(direct = true) {
+        this.reverse = !direct;
+    }
 
-  encrypt(message, key) {
-    if (arguments.length < 2)
-      throw new Error('Expected 2 arguments: message and key.');
+    encrypt(message, key) {
+        if (arguments.length < 2)
+            throw new Error('Expected 2 arguments: message and key.');
 
-    key = key.toUpperCase();
-    let skippedCount = 0;
-    const encrypted = message.toUpperCase()
-        .split('')
-        .map((letter, i) => {
-          if (!this.ALPHABET.includes(letter)) {
-            ++skippedCount;
-            return letter;
-          }
-          const keyShift = key.charCodeAt((i - skippedCount) % key.length) - this.base;
-          const newLetter = (letter.charCodeAt(0) + keyShift);
-          if (newLetter <= this.max.charCodeAt(0))
-              return this.ALPHABET[newLetter - this.base];
-          else
-              return this.ALPHABET[newLetter - this.max.charCodeAt(0) - 1];
-        });
+        key = key.toUpperCase();
+        let skippedCount = 0;
+        const encrypted = message.toUpperCase()
+            .split('')
+            .map((letter, i) => {
+                if (!this.ALPHABET.includes(letter)) {
+                    ++skippedCount;
+                    return letter;
+                }
+                const keyShift = key.charCodeAt((i - skippedCount) % key.length) - this.base;
+                const newLetter = (letter.charCodeAt(0) + keyShift);
+                if (newLetter <= this.max.charCodeAt(0))
+                    return this.ALPHABET[newLetter - this.base];
+                else
+                    return this.ALPHABET[newLetter - this.max.charCodeAt(0) - 1];
+            });
 
-    return this.reverse ? encrypted.reverse().join('') : encrypted.join('');
-  }
+        return this.reverse ? encrypted.reverse().join('') : encrypted.join('');
+    }
 
-  decrypt(message, key) {
-    if (arguments.length < 2)
-      throw new Error('Expected 2 arguments: message and key.');
+    decrypt(message, key) {
+        if (arguments.length < 2)
+            throw new Error('Expected 2 arguments: message and key.');
 
-    key = key.toUpperCase();
-    let skippedCount = 0;
-    const decrypted = message.toUpperCase()
-        .split('')
-        .map((letter, i) => {
-          if (!this.ALPHABET.includes(letter)) {
-            ++skippedCount;
-            return letter;
-          }
-          const keyShift = key.charCodeAt((i - skippedCount) % key.length) - this.base;
-          const newLetter = (letter.charCodeAt(0) - keyShift);
-          if (newLetter >= 'A'.charCodeAt(0))
-            return this.ALPHABET[newLetter - this.base];
-          else
-            return this.ALPHABET[newLetter + this.ALPHABET.length - this.base];
-        });
+        key = key.toUpperCase();
+        let skippedCount = 0;
+        const decrypted = message.toUpperCase()
+            .split('')
+            .map((letter, i) => {
+                if (!this.ALPHABET.includes(letter)) {
+                    ++skippedCount;
+                    return letter;
+                }
+                const keyShift = key.charCodeAt((i - skippedCount) % key.length) - this.base;
+                const newLetter = (letter.charCodeAt(0) - keyShift);
+                if (newLetter >= 'A'.charCodeAt(0))
+                    return this.ALPHABET[newLetter - this.base];
+                else
+                    return this.ALPHABET[newLetter + this.ALPHABET.length - this.base];
+            });
 
-    return this.reverse ? decrypted.reverse().join('') : decrypted.join('');
-  }
+        return this.reverse ? decrypted.reverse().join('') : decrypted.join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
